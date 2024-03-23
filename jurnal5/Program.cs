958a9fb4c5c8bc73b28f248776582575d077113e
Fauzan <fauzan06arrizqy@gmail.com>
2024-03-23 08:51:17 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- jurnal5/Program.cs
+++ jurnal5/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal5 {
+class Penjumlahan<T>
+{
+    public dynamic JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        dynamic A = angka1;
+        dynamic B = angka2;
+        dynamic C = angka3;
+
+        return A + B + C;
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Penjumlahan<int> JumlahTigaAngka = new Penjumlahan<int>();
+        Console.WriteLine(JumlahTigaAngka.JumlahTigaAngka(13, 02, 22));
+    }
+}
+}
--- jurnal5/obj/Debug/net8.0/jurnal5.AssemblyInfo.cs
+++ jurnal5/obj/Debug/net8.0/jurnal5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jurnal5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92dacd1091a257fd79f58e61165e21c780cefa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("jurnal5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jurnal5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
